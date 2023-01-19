12425164fff2dbfdf60f7a0dc74d7221e6cf0ef2
David_Martinez <javidx24@icloud.com>
2023-01-18 20:16:26 -0500

    working on text padding

--- QR Code Component/src/QR/QR.jsx
+++ QR Code Component/src/QR/QR.jsx
@@ -4,10 +4,10 @@ import QrCode from "../../../images/image-qr-code.png"
 const QR = () => {
     return (
         <>
-            <div className=" h-80 p-4 bg-white rounded-lg">
+            <div className=" h-auto p-4 bg-white rounded-lg">
                 <img className="rounded-lg w-52" src={QrCode} alt='qr-code-image' />
-                <h2 className="w-52 text-15px font-outfit text-Dark-blue">Improve your front-end skills by building projects</h2>
-                <p className="w-52 text-13px font-outfit  text-Grayish-blue">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                <h2 className="w-52 text-15px font-outfit text-Dark-blue px-2">Improve your front-end skills by building projects</h2>
+                <p className="w-52 text-13px font-outfit  text-Grayish-blue px-3">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </>
     )
