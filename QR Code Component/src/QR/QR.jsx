45ade92f20be8715ab29306452a276472a26f47b
David_Martinez <javidx24@icloud.com>
2023-01-19 19:32:01 -0500

    add deploy workflow

--- QR Code Component/src/QR/QR.jsx
+++ QR Code Component/src/QR/QR.jsx
@@ -4,10 +4,10 @@ import QrCode from "../../../images/image-qr-code.png"
 const QR = () => {
     return (
         <>
-            <div className=" h-auto p-4 bg-white rounded-lg">
+            <div className=" h-auto p-3 bg-white rounded-lg">
                 <img className="rounded-lg w-52" src={QrCode} alt='qr-code-image' />
                 <h2 className="w-52 text-15px font-outfit text-Dark-blue px-2 pt-2">Improve your front-end skills by building projects</h2>
-                <p className="w-52 text-11px font-outfit  text-Grayish-blue px-3 pt-2 ">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+                <p className="w-52 text-11px font-outfit  text-Grayish-blue p-3 ">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </>
     )
